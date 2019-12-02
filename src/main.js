6eaeae3eb41dd5c1e6e9aa4c0dad05c33792f27e
Josh Bryan <joshintosh.bryan@gmail.com>
2019-12-02 06:10:31 -0600

    Init

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-/*new Vue({
-  render: h => h(App)
-}).$mount("#app");*/
-
 new Vue({
+  render: h => h(App)
+}).$mount("#app");
+
+/*new Vue({
   el: "#app",
   template: "<App />",
   components: {
     App
   }
-});
+});*/
